1df6f21d7d809eefec8b8472369ae434bdc1312a
taiki_one <taiki.one4k@gmail.com>
2015-10-02 14:51:29 +0000

    add nicovideo, comments on home/index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,13 @@
 class HomeController < ApplicationController
+	def index
+        @id = params[:id]
+        if @id == nil then
+            @id = "sm18391671" #sm25019253 #ebifly #sm9704169 #bond
+        end 
+
+        cookie = login_nicovideo(NICOADD, NICOPASS)
+        flv_info = get_flv_info(cookie, @id)
+        @comments = get_comments(flv_info, 10) # max 1000
+
+	end
 end
