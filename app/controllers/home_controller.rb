308c9cd1714889803126a12f65223c2447ad2aa0
taiki_one <taiki.one4k@gmail.com>
2015-10-02 15:16:03 +0000

    set ENV[NICO*]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
             @id = "sm18391671" #sm25019253 #ebifly #sm9704169 #bond
         end 
 
-        cookie = login_nicovideo(NICOADD, NICOPASS)
+        cookie = login_nicovideo(ENV["NICOADD"], ENV["NICOPASS"])
         flv_info = get_flv_info(cookie, @id)
         @comments = get_comments(flv_info, 10) # max 1000
 
