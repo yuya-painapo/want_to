9bc49a74141df420c582a0c1a4cc24cdb479d2f0
taiki_one <taiki.one4k@gmail.com>
2015-10-15 07:53:22 +0000

    Chenge notice massage home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -76,7 +76,7 @@ class HomeController < ApplicationController
 
   def search
 	if params[:q].empty? then
-		 flash[:notice] = '盛り上がってる動画が見つからないよぉ'
+		 flash[:notice] = '盛り上がってる動画が見つからないの'
 	 	 redirect_to action: 'index'
     elsif params[:q].match(/^sm[0-9]*$/) then
       redirect_to action: 'movie', id: params[:q]
@@ -88,7 +88,7 @@ class HomeController < ApplicationController
 	  	smID = results[rand(results.size)].cmsid
 	  	redirect_to action: 'movie', id: smID
 	  else
-		 flash[:notice] = '盛り上がってる動画が見つからないよぉ'
+		 flash[:notice] = '盛り上がってる動画が見つからないの'
 	 	 redirect_to action: 'index'
 	  end
   end
