947cafaee82839ac8b31baff3ffb0ec1062e5c16
hogehiga <hogehiga@gmail.com>
2015-10-22 02:26:24 +0900

    Test whether the graph is divided.

--- spec/steps/common_steps.rb
+++ spec/steps/common_steps.rb
@@ -11,6 +11,10 @@ step ':movie_id の再生画面が表示されていること' do |movie_id|
   expect(current_path).to eq "/home/movie/#{movie_id}"
 end
 
+step ':movie_id の :n 分割再生画面が表示されていること' do |movie_id, n|
+  expect(current_path).to eq "/home/movie/#{movie_id}/#{n}"
+end
+
 step 'メッセージ :message が表示されていること' do |message|
   expect(page).to have_content message
 end
@@ -21,4 +25,8 @@ end
 
 step '要素 :selector が存在すること' do |selector|
   expect(page.has_css?(selector, visible: :all)).to eq true
+end
+
+step '要素 :selector が :n 個存在すること' do |selector, n|
+  expect(page.has_css?(selector, count: n, visible: :all)).to eq true
 end
