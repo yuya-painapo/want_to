514fc9bd2e27843cd51bb381732d9754c032fa25
hogehiga <hogehiga@gmail.com>
2015-10-31 19:53:44 +0900

    Remove rubocop warnings.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,13 +14,14 @@ class HomeController < ApplicationController
     https = Net::HTTP.new(host, 443)
     https.use_ssl = true
     https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    response = https.start { |https|
-      https.post(path, body)
+    response = https.start { |h|
+      h.post(path, body)
     }
     
     cookie = ''
     response['set-cookie'].split('; ').each do |st|
-      if idx = st.index('user_session_')
+      idx = st.index('user_session_')
+      if idx
         cookie = "user_session=#{st[idx..-1]}"
         break
       end
@@ -49,7 +50,7 @@ class HomeController < ApplicationController
   
   def get_comments(flv_info, res_from)
     host = URI.unescape(flv_info[:ms])
-    request = "#{host.gsub(/\/api\//, '/api.json/')}thread?thread=#{flv_info[:thread_id]}&version=20061206&res_from=-#{res_from.to_s}"
+    request = "#{host.gsub(/\/api\//, '/api.json/')}thread?thread=#{flv_info[:thread_id]}&version=20061206&res_from=-#{res_from}"
     
     uri = URI.parse(request)
     json = Net::HTTP.get(uri)
@@ -183,7 +184,7 @@ class HomeController < ApplicationController
     doc = Nokogiri::HTML.parse(html, nil, charset)
     tagRank = Hash.new
 
-	doc.css('div#tagRanking/div.box/h2').each do |node|
+    doc.css('div#tagRanking/div.box/h2').each do |node|
         rank = node.css('span').inner_text.to_i
         tag = node.css('a').inner_text
         tagRank.store(rank, tag)
