9c625f42981bffcc0ad8a6b5c93e5eb452e4ec42
hogehiga <hogehiga@gmail.com>
2015-11-11 23:13:08 +0900

    Semantic merge.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,4 +6,10 @@ FactoryGirl.define do
     password "userpassword"
     password_confirmation "userpassword"
   end
+
+  factory :bookmark_user, class: User do
+    email "test@example.com"
+    password "hogehoge"
+    encrypted_password "$2a$10$8fCNvyqJICPbHRptYKukfejPnEmwl9ISi7gaZ/s33E0nRnTIm4f7O"
+  end
 end
