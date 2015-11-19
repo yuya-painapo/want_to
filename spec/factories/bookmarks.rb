5eb3710ae4568a76e73fc506e996d385efc7b6be
hogehiga <hogehiga@gmail.com>
2015-11-20 03:57:16 +0900

    Refactoring.
    Change comment for easy-to-detect.

--- spec/factories/bookmarks.rb
+++ spec/factories/bookmarks.rb
@@ -3,21 +3,21 @@ FactoryGirl.define do
   factory :bookmark_user2_sm9_1, class: Bookmark do
     smid "sm9"
     start_vpos 3000
-    comment 'コメントです'
+    comment 'bookmark_user2_sm9_1'
 
     factory :bookmark_user2_sm9_2, class: Bookmark do
       start_vpos 13000
-      comment 'コメント2です'
+      comment 'bookmark_user2_sm9_2'
     end
   end
 
   factory :bookmark_user1_sm13_1, class: Bookmark do
     smid "sm13"
     start_vpos 3000
-    comment 'コメントです'
+    comment 'bookmark_user1_sm13_1'
 
     factory :bookmark_user2_sm13_1, class: Bookmark do
-      comment 'bookmark_sm13_2'
+      comment 'bookmark_user2_sm13_1'
     end
   end
 end
