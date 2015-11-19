b0c338f52c206e76a7b93e58c1c7fe8d60676dd3
hogehiga <hogehiga@gmail.com>
2015-11-20 03:50:40 +0900

    Refactoring.
    Add username to factory name.

--- spec/factories/bookmarks.rb
+++ spec/factories/bookmarks.rb
@@ -1,22 +1,22 @@
 # coding: utf-8
 FactoryGirl.define do
-  factory :bookmark_sm9, class: Bookmark do
+  factory :bookmark_user2_sm9, class: Bookmark do
     smid "sm9"
     start_vpos 3000
     comment 'コメントです'
 
-    factory :bookmark_sm9_2, class: Bookmark do
+    factory :bookmark_user2_sm9_2, class: Bookmark do
       start_vpos 13000
       comment 'コメント2です'
     end
   end
 
-  factory :bookmark_sm13, class: Bookmark do
+  factory :bookmark_user1_sm13, class: Bookmark do
     smid "sm13"
     start_vpos 3000
     comment 'コメントです'
 
-    factory :bookmark_sm13_2, class: Bookmark do
+    factory :bookmark_user2_sm13_2, class: Bookmark do
       comment 'bookmark_sm13_2'
     end
   end
