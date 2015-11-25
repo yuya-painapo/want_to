e70dde675715d91ec9f65f50508b22a74fbde62f
taiki_one <taiki.one4k@gmail.com>
2015-11-25 11:22:37 +0000

    sign in email or nickname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,16 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   protected
 
+#  def configure_permitted_parameters
+#    #devise_parameter_sanitizer.for(:sign_in) << :nickname
+#    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:nickname, :email, :password) }
+#    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nickname, :email, :password, :password_confirmation) }
+#  end
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) << :nickname
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nickname, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nickname, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :nickname, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:nickname, :email, :password, :password_confirmation, :current_password) }
   end
 
   protect_from_forgery with: :exception
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :login
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
   has_many :bookmarks, dependent: :destroy
   validates :nickname, presence: true, uniqueness: true
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["nickname= :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
