3c54e4ed00bf2cb6ab77549c812712ecbbeb6513
taiki_one <taiki.one4k@gmail.com>
2015-11-30 14:38:11 +0000

    add settings heroku to use sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,19 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
  
   config.action_mailer.default_url_options = { :host => 'fathomless-mesa-6023.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
