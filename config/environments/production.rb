4e847ebdb824f1063f0ada35c7e8ce06e9cbe0ee
yuya-painapo <k158578@ie.u-ryukyu.ac.jp>
2015-12-05 03:29:03 +0000

    fix host email address for setting password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
  
-  config.action_mailer.default_url_options = { :host => 'niconico-hightlight.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'niconico-highlight.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
