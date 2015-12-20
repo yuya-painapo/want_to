6e4694bf5ce0a376d05be83682d6ef9af238acfd
yuya-nakamura <yuyan7sh@gmail.com>
2015-12-21 00:41:16 +0900

    MyPageのふせん一覧をテーブル化 ふせんの編集ボタンを追加

--- app/controllers/my_page_controller.rb
+++ app/controllers/my_page_controller.rb
@@ -1,8 +1,27 @@
+# MyPageController
 class MyPageController < ApplicationController
-
   before_action :authenticate_user!
 
   def index
     @bookmarks = Bookmark.where(user_id: current_user.id)
+  end
+
+  def update
+    bookmark = Bookmark.where(user_id: current_user.id, id: params[:id]).first
+    bookmark.start_vpos = params[:start_vpos]
+
+    bookmark.comment = params[:comment]
+    bookmark.save
+
+    redirect_to action: 'index'
+  end
+
+  def delete
+    bookmark = Bookmark.where(user_id: current_user.id, id: params[:id]).first
+    if bookmark.destroy
+      flash[:notice] = 'ふせんを削除しました。'
+    else
+      flash[:notice] = 'ふせんの削除に失敗しました。'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'user/show'
   get 'my_page/index'
+  post 'my_page/update'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
