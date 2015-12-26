c532b36f50ff8a87722124b933bae036f0588120
yuya-nakamura <yuyan7sh@gmail.com>
2015-12-26 15:11:13 +0900

    create follow table relation user and follow

---
+++ app/models/follow.rb
@@ -0,0 +1,5 @@
+# Follow Model
+class Follow < ActiveRecord::Base
+  belongs_to :from_user, class_name: User, foreign_key: :from_user_id
+  belongs_to :to_user, class_name: User, foreign_key: :to_user_id
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# User Model
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -5,6 +6,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable,
          :rememberable, :trackable, :validatable, :omniauthable, authentication_keys: [:login]
   has_many :bookmarks, dependent: :destroy
+  has_many :follows_from, class_name: Follow, foreign_key: :from_user_id, dependent: :destroy
+  has_many :follows_to, class_name: Follow, foreign_key: :to_user_id, dependent: :destroy
+  has_many :following, through: :follows_from, source: :to_user
+  has_many :followed, through: :follows_to, source: :from_user
   validates :nickname, presence: true, uniqueness: true
 
   def self.find_for_facebook_oauth(auth)
@@ -33,8 +38,16 @@ class User < ActiveRecord::Base
       where(conditions).find_by(['nickname= :value OR lower(email) = lower(:value)', { value: login }])
     end
   end
- 
+
   def self.dummy_email(auth)
     "#{auth.uid}-#{auth.provider}@example.com"
+  end
+
+  def following?(user)
+    following.where(id: user.id).any?
+  end
+
+  def followed_by?(user)
+    followed.where(id: user.id).any?
   end
 end
